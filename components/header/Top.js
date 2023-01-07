3342678a3f30eb2ac2b34e27ed7aaee134bbadeb
Mehdi Hajikhani <mhaji007@fiu.edu>
2023-01-06 20:43:44 -0800

    Create UserMenu component. Add initial styles

--- components/header/Top.js
+++ components/header/Top.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styles from "./styles.module.scss";
 import { MdSecurity } from "react-icons/md";
 import { BsSuitHeart } from "react-icons/bs";
@@ -5,8 +6,10 @@ import { RiAccountPinCircleLine, RiArrowDropDownFill } from "react-icons/ri";
 import Link from "next/link";
 import Image from "next/image";
 import flag from "../../public/images/flag.png";
+import UserMenu from "./UserMenu";
 
 export default function Top() {
+  const [loggedIn, setLoggedIn] = useState(true);
   return (
     <div className={styles.top}>
       <div className={styles.top__container}>
@@ -32,12 +35,30 @@ export default function Top() {
               <span>Whishlist</span>
             </Link>
           </li>
-          <li className={styles.li}>
-            <div className={styles.flex}>
-              <RiAccountPinCircleLine />
-              <span>Account</span>
-              <RiArrowDropDownFill />
-            </div>
+          {/* Wrapping the two list item inside another li
+          allows for more flexiblity in styling the elements inside */}
+          <li>
+            {loggedIn ? (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <img
+                    src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png"
+                    alt="temp avatar"
+                  />
+                  <span>Mehdi Hajikhani</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            ) : (
+              <li className={styles.li}>
+                <div className={styles.flex}>
+                  <RiAccountPinCircleLine />
+                  <span>Account</span>
+                  <RiArrowDropDownFill />
+                </div>
+              </li>
+            )}
+            <UserMenu loggedIn={loggedIn} />
           </li>
         </ul>
       </div>
---
+++ components/header/UserMenu.js
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import styles from "./styles.module.scss";
+
+export default function UserMenu({ loggedIn }) {
+  return (
+    <div className={styles.menu}>
+      <h4>Welcome to Trade Zone</h4>
+      {loggedIn ? (
+        <div className={styles.flex}>
+          <img
+            src="https://e7.pngegg.com/pngimages/799/987/png-clipart-computer-icons-avatar-icon-design-avatar-heroes-computer-wallpaper.png"
+            alt=""
+            className={styles.menu__img}
+          />
+          <div className={styles.col}>
+            <span>Welcome Back,</span>
+            <h3>Mehdi</h3>
+            <span onClick={() => signOut()}>Sign out</span>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.flex}>
+          <button className={styles.btn_primary}>Register</button>
+          <button className={styles.btn_outlined} onClick={() => signIn()}>
+            Login
+          </button>
+        </div>
+      )}
+      <ul>
+        <li>
+          <Link href="/profile">Account</Link>
+        </li>
+        <li>
+          <Link href="/profile/orders">My Orders</Link>
+        </li>
+        <li>
+          <Link href="/profile/messages">Message Center</Link>
+        </li>
+        <li>
+          <Link href="/profile/address">Address</Link>
+        </li>
+        <li>
+          <Link href="/profile/whishlist">Whishlist</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
