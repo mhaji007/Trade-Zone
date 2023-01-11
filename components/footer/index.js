6d3294647af7eb62feae137d601dcbaa5853ef8f
Mehdi Hajikhani <mhaji007@fiu.edu>
2023-01-10 18:18:14 -0800

    Create the Socials and the NewsLetter components

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,4 +1,6 @@
 import Links from "./Links";
+import NewsLetter from "./NewsLetter";
+import Socials from "./Socials";
 import styles from "./styles.module.scss";
 
 export default function Footer({ country }) {
@@ -6,6 +8,8 @@ export default function Footer({ country }) {
     <footer className={styles.footer}>
       <div className={styles.footer__container}>
         <Links />
+        <Socials />
+        <NewsLetter />
       </div>
     </footer>
   );
