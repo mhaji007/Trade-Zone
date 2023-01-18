7684bbba542436d5eaefc91b5e7bc68c0e347a1e
Mehdi Hajikhani <mhaji007@fiu.edu>
2023-01-17 18:23:25 -0800

    Set up Auth0 provider

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,6 +3,7 @@ import TwitterProvider from "next-auth/providers/twitter";
 import FacebookProvider from "next-auth/providers/facebook";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import Auth0Provider from "next-auth/providers/auth0";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import clientPromise from "./lib/mongodb";
 
@@ -25,6 +26,19 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
+      // Enables automatic account linking.
+      // Note:
+      // Normally, when you sign in with an OAuth provider and another account with the same email address
+      // already exists, the accounts are not linked automatically. Automatic account linking on sign in
+      // is not secure between arbitrary providers and is disabled by default. However,
+      // it may be desirable to allow automatic account linking if you trust that the provider involved has
+      // securely verified the email address associated with the account.
+      allowDangerousEmailAccountLinking: true,
     }),
   ],
   pages: {
