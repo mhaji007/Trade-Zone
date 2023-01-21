0a53f46402bb99caf28bc46be882d42b3a8758b0
Mehdi Hajikhani <mhaji007@fiu.edu>
2023-01-20 22:04:59 -0800

    Set up custom input with Yup

--- components/inputs/loginInput/index.js
+++ components/inputs/loginInput/index.js
@@ -2,10 +2,17 @@ import styles from "./styles.module.scss";
 import { BiUser } from "react-icons/bi";
 import { SiMinutemailer } from "react-icons/si";
 import { IoKeyOutline } from "react-icons/io5";
+import { ErrorMessage, useField } from "formik";
 
-export default function LoginInput({ icon, placeholder }) {
+export default function LoginInput({ icon, placeholder, ...props }) {
+  const [field, meta] = useField(props);
+  console.log("meta", meta);
   return (
-    <div className={styles.input}>
+    <div
+      className={`${styles.input} ${
+        meta.touched && meta.error ? styles.error : ""
+      }`}
+    >
       {icon == "user" ? (
         <BiUser />
       ) : icon == "email" ? (
@@ -15,7 +22,19 @@ export default function LoginInput({ icon, placeholder }) {
       ) : (
         ""
       )}
-      <input type="text" placeholder={placeholder} />
+      <input
+        type={field.type}
+        name={field.name}
+        placeholder={placeholder}
+        {...field}
+        {...props}
+      />
+      {meta.touched && meta.error && (
+        <div className={styles.error__popup}>
+          <span></span>
+          <ErrorMessage name={field.name} />
+        </div>
+      )}
     </div>
   );
 }
--- pages/signin.js
+++ pages/signin.js
@@ -1,12 +1,41 @@
+import { useState } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import styles from "../styles/signin.module.scss";
 import { BiLeftArrowAlt } from "react-icons/bi";
 import Link from "next/link";
 import { Formik, Form } from "formik";
+import * as Yup from "yup";
 import LoginInput from "../components/inputs/loginInput";
 
+const initialvalues = {
+  login_email: "",
+  login_password: "",
+  name: "",
+  email: "",
+  password: "",
+  conf_password: "",
+  success: "",
+  error: "",
+  login_error: "",
+};
+
 export default function signin() {
+  const [user, setUser] = useState(initialvalues);
+  const { login_email, login_password } = user;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("Email address is required.")
+      .email("Please enter a valid email address."),
+    login_password: Yup.string().required("Please enter a password"),
+  });
+
   const country = {
     name: "United States",
     flag: "https://en.wikipedia.org/wiki/Flag_of_the_United_States#/media/File:Flag_of_the_United_States.svg",
@@ -28,13 +57,29 @@ export default function signin() {
           <div className={styles.login__form}>
             <h1>Sign in</h1>
             <p>Get access to tons of unique deals.</p>
-            <Formik>
+            <Formik
+              enableReinitialize
+              initialValues={{
+                login_email,
+                login_password,
+              }}
+              validationSchema={loginValidation}
+            >
               {(form) => (
                 <Form method="post" action="/api/auth/signin/email">
                   <LoginInput
                     type="text"
+                    name="login_email"
                     icon="email"
                     placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
                   />
                 </Form>
               )}
