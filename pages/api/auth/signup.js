4a43888443552f16bf4f51b64f464b32a38f919e
Mehdi Hajikhani <mhaji007@fiu.edu>
2023-01-26 11:11:51 -0800

    Hash password. Create activation token

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,6 +1,12 @@
 import nc from "next-connect";
 import { validateEmail } from "../../../utils/validation";
 import db from "../../../utils/db";
+import { createActivationToken } from "../../../utils/tokens";
+
+// brcypt is just a (one of many) algorithms to one-way hash a password. So it's generally used in a login mechanism; you hash the login password with bcrypt and then compare it to the already hashed password in your database.
+
+// JWT comes into play after a login is succesful. It's a mechanism where the client, after a succesful login, receives a JWT token that contains information who he is (authentication) and what he is allowed to do (authorization). This token is signed, so the client can pass this token in to other services that can verify the signature to make sure the client didn't fake it.
+
 const handler = nc();
 
 handler.post(async (req, res) => {
@@ -20,6 +26,13 @@ handler.post(async (req, res) => {
         .status(400)
         .json({ message: "Password must be at least 6 characters long." });
     }
+    const cryptedPassword = await bcrypt.hash(password, 12);
+    const newUser = new User({ name, email, password: cryptedPassword });
+
+    const addedUser = await newUser.save();
+    const activation_token = createActivationToken({
+      id: addedUser._id.toString(),
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
