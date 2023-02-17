d126aa740f14f3a5b3eedbb554b0c05970a75632
Mehdi Hajikhani <mhaji007@fiu.edu>
2023-02-16 20:25:39 -0800

    Create the offers component

---
+++ data/home.js
@@ -0,0 +1,254 @@
+export const offersAarray = [
+  {
+    image:
+      "https://ae01.alicdn.com/kf/H7026c7b0e94641b197a629356be07928y.jpg_220x220.webp_480x480Q90.webp",
+    price: "144.37",
+    discount: "17",
+  },
+
+  {
+    image:
+      "https://ae01.alicdn.com/kf/S02af1ff99faa46858dd1156cebd9986cj.jpg_220x220.webp_480x480Q90.webp",
+    price: "12.76",
+    discount: "19",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/S2c3ed57a2ca84a0ea3666ae9ddb15d97s.jpg_220x220.webp_480x480Q90.webp",
+    price: "4.09",
+    discount: "32",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/Se5174cc7f58d445d9c33f628d61cd23cm.jpg_220x220.webp_480x480Q90.webp",
+    price: "9.61",
+    discount: "90",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/S32ed35b0d0974171996af11e6c44d065T.jpg_220x220.webp_480x480Q90.webp",
+    price: "6.25",
+    discount: "45",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/Sba6e2cc9d2ce4bf986c34d353dfdd5ecF.jpg_220x220.webp_480x480Q90.webp",
+    price: "13.74",
+    discount: "10",
+  },
+  {
+    image:
+      "https://ae01.alicdn.com/kf/Sb462e23c63664774ab37abfc0f3a29daT.jpg_220x220.webp_480x480Q90.webp",
+    price: "198.2",
+    discount: "5",
+  },
+];
+export const menuArray = [
+  {
+    name: "Women's Fashion",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Men's Fashion",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Electronics",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Jewlery & Watches",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Home, Pet & Appliances",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Beauty, Health & Hair",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+
+  {
+    name: "Shoes,Sneakers,Heels",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+
+  {
+    name: "Accessories",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+
+  {
+    name: "Sports & Entertainment ",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Kids & Babies",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Movies & Television",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Gaming, Video Games",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Phones & Telecommunications",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Toys & Hobbies",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Gifts & Crafts",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Machinery",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+  {
+    name: "Security, Safety",
+    link: "",
+    subMenu: [
+      {
+        name: "",
+        link: "",
+      },
+    ],
+    brands: [],
+    images: [],
+  },
+];
