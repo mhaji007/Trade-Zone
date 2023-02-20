810c3726eab90cfac6e6678d97cb6175132e4d1f
Mehdi Hajikhani <mhaji007@fiu.edu>
2023-02-19 19:23:52 -0800

    Create the Home User Menu component

--- components/home/main/index.js
+++ components/home/main/index.js
@@ -2,6 +2,7 @@ import Menu from "./Menu";
 import Offers from "./offers";
 import styles from "./styles.module.scss";
 import MainSwiper from "./swiper";
+import User from "./User";
 
 export default function Main() {
   return (
@@ -10,7 +11,7 @@ export default function Main() {
       <Menu />
       <MainSwiper />
       <Offers />
-      <div className={styles.user}>user</div>
+      <User />
     </div>
   );
 }
